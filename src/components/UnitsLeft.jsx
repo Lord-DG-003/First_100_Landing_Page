0c74c4063586afaafe4b9da0840e1da51e3f9a0a
Mridul Pal <mridulpal04@gamil.com>
2026-08-04 20:02:29 +0530

    Updated UnitsLeft

--- src/components/UnitsLeft.jsx
+++ src/components/UnitsLeft.jsx
@@ -7,7 +7,7 @@ const UnitsLeft = () => {
         <div className='rounded-full h-21 w-21 shrink-0 items-center border justify-center flex mx-5 border-[#1f232a] bg-[#16181d]'>
             <Box className='size-15 text-[#d4af37]'/>
         </div>
-        <div className='flex flex-col  pl-4 justify-center w-5/8 border-l border-[#1f232a]'>
+        <div className='flex flex-col  pl-4 justify-center w-5/8 border-l border-[#1f232a] my-5'>
             <p className='text-[16px] text-[#d4af37]'>LIMITED TO 2000 UNITS</p>
             <p className='text-5xl'>1,842</p>
             <p className='text-[18px] text-[#d4af37]'>UNITS LEFT</p>
