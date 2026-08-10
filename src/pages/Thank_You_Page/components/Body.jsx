65ea39cf8322746454f47af8fa3c69779eae5d17
Mridul Pal <mridulpal04@gamil.com>
2026-08-10 17:26:25 +0530

    Thank You page mobile view is improved!

--- src/pages/Thank_You_Page/components/Body.jsx
+++ src/pages/Thank_You_Page/components/Body.jsx
@@ -4,7 +4,7 @@ import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 
 const Body = () => {
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center mt-3'>
+    <div className='flex flex-col items-center justify-center mt-3'>
         <IoIosCheckmarkCircleOutline size={60} className='text-[#d1a44e]'/>
         <h1 className='text-[#ffffff] text-5xl md:text-6xl mt-3 font-semibold font-satoshi'>
             Thank You!
@@ -15,39 +15,39 @@ const Body = () => {
         <p className='text-[#a6a9b1] -mt-1 text-base md:text-lg font-inter'>
             Your ebook is ready to download.
         </p>
-        <div className=" bg-[#0d0e11]">
+        <div className=" bg-black">
       
-      {/* Book container */}
-      <div className="relative w-63 bg-linear-to-br from-[#1a1a1c] to-[#111112] rounded-l-md rounded-r-xl shadow-[20px_20px_50px_rgba(0,0,0,0.8),inset_1px_1px_2px_rgba(255,255,255,0.05)] pt-6 px-6 mt-5 overflow-hidden">
+          {/* Book container */}
+          <div className="relative w-63 bg-linear-to-br from-[#1a1a1c] to-[#111112] rounded-l-md rounded-r-xl     shadow-[20px_20px_50px_rgba(0,0,0,0.8),inset_1px_1px_2px_rgba(255,255,255,0.05)] pt-6 px-6 mt-5 overflow-hidden">
         
-        {/* Book spine */}
-        <div className="absolute left-0 top-0 bottom-0 w-5 bg-linear-to-r from-[#0a0a0b] via-[#222225] to-[#161618] border-r border-black/50"></div>
+          {/* Book spine */}
+          <div className="absolute left-0 top-0 bottom-0 w-5 bg-linear-to-r from-[#0a0a0b] via-[#222225] to-[#161618] border-r border-black/50"></div>
         
-        {/* Content wrapper */}
-        <div className="ml-2.5 flex flex-col">
+          {/* Content wrapper */}
+          <div className="ml-2.5 flex flex-col">
           
-          <h2 className="text-white text-3xl font-semibold tracking-[0.02em] mb-1.5 [text-shadow:0px_4px_10px_rgba(0,0,0,0.5)] font-satoshi">
-            MAKE YOUR
-          </h2>
+            <h2 className="text-white text-3xl font-semibold tracking-[0.02em] mb-1.5 [text-shadow:0px_4px_10px_rgba(0,0,0,0.5)] font-satoshi">
+              MAKE YOUR
+            </h2>
           
-          <h1 className="text-[#d1a44e] text-[3rem] font-bold leading-[1.05]   [text-shadow:0px_10px_20px_rgba(0,0,0,0.6),0px_2px_4px_rgba(0,0,0,0.3)] font-inter mb-3">
-            FIRST<br />
-            $100.
-          </h1>
+            <h1 className="text-[#d1a44e] text-[3rem] font-bold leading-[1.05]   [text-shadow:0px_10px_20px_rgba(0,0,0,0.6),0px_2px_4px_rgba(0,0,0,0.3)] font-inter mb-3">
+              FIRST<br />
+              $100.
+            </h1>
           
-          <p className="text-[#d1d1d1] text-[0.82rem] leading-[1.6] font-normal mb-7 font-inter">
-            A practical guide for students<br />
-            to earn their first $100 online -<br />
-            no experience, no fluff.
-          </p>
+            <p className="text-[#d1d1d1] text-[0.82rem] leading-[1.6] font-normal mb-7 font-inter">
+              A practical guide for students<br />
+              to earn their first $100 online -<br />
+              no experience, no fluff.
+            </p>
           
-          <div className="w-30 h-[2.5px] bg-[#d1a44e] rounded-sm mb-8"></div>
+            <div className="w-30 h-[2.5px] bg-[#d1a44e] rounded-sm mb-8"></div>
           
+          </div>
         </div>
       </div>
-    </div>
           <Download/>
-    </div>
+      </div>
   )
 }
 
--- src/pages/Thank_You_Page/components/PurchaseNotice.jsx
+++ src/pages/Thank_You_Page/components/PurchaseNotice.jsx
@@ -4,23 +4,24 @@ import { LuShieldCheck } from "react-icons/lu";
 
 const PurchaseNotice = () => {
 
-    const Logos=[<HiOutlineMail className='text-[#d1a44e] md:size-12 size-10' />,<LuShieldCheck className='text-[#d1a44e] md:size-12 size-10' />];
+    const Logos=[<HiOutlineMail className='text-[#d1a44e] md:size-12 size-9' />,<LuShieldCheck className='text-[#d1a44e] md:size-12 size-9' />];
     const Titles=["We've sent your ebook to your email.","No refunds on digital products."];
-    const Descriptions=["Didn't receive it? Check your spam folder or contact support.","Please read our Terms of Use."];
+    const Descriptions=["Didn't receive it? Check your spam folder or ","Please read our "];
+    const Links=["contact support.","Terms of Use."]
 
   return (
     <div className='flex flex-col justify-center items-center mb-5 font-inter'>
         {Logos.map((Logo,index)=>(
-            <div className='border border-[#1f232a] flex items-center h-fit w-120 mt-5 py-4 px-4 rounded-md'>
-                <div className='h-18 w-18 rounded-full shrink-0 bg-[#161616] flex items-center justify-center mr-1.5'>
+            <div className='border border-[#1f232a] flex items-center h-fit w-92 md:w-120 mt-5 py-4 px-0 rounded-md'>
+                <div className='h-15 w-15 rounded-full shrink-0 bg-[#161616] flex items-center justify-center md:mx-5 mx-1'>
                     {Logo}
                 </div>
                <div className='border-l border-[#1f232a]'>
-                    <h3 className='text-[#ffffff] font-semibold ml-4'>
+                    <h3 className='text-[#ffffff] font-semibold ml-4 text-[15.2px]'>
                         {Titles[index]}
                     </h3>
-                    <p className='text-[#a6a9b1] ml-4'>
-                        {Descriptions[index]}
+                    <p className='text-[#a6a9b1] ml-4 text-[14px] md:pr-6'>
+                        {Descriptions[index]} <a href="#" className='text-[#d4af37] cursor-pointer hover:text-[#f5be08]'>{Links[index]}</a>
                     </p>
                </div>
             </div>
